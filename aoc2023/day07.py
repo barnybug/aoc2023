7068b6288578fdd10c390eac7fdb81dfa7a01d4e
Barnaby Gray <barnaby@pickle.me.uk>
2023-12-11 08:02:35 +0000

    Day 7

---
+++ aoc2023/day07.py
@@ -0,0 +1,50 @@
+from collections import Counter
+
+from .utils import Answer
+
+VALUES = {str(i): i for i in range(2, 10)} | {c: i + 10 for i, c in enumerate("TJQKA")}
+VALUES2 = VALUES | {"J": 1}
+
+
+class Hand:
+    def __init__(self, line):
+        self.cards, bid = line.split(" ")
+        self.bid = int(bid)
+
+
+def card_values(hand, most, second, values):
+    handtype = {
+        (5, 0): 6,
+        (4, 1): 5,
+        (3, 2): 4,
+        (3, 1): 3,
+        (2, 2): 2,
+        (2, 1): 1,
+        (1, 1): 0,
+    }[(most, second)]
+    return [handtype] + [values[c] for c in hand.cards]
+
+
+def key(hand: Hand):
+    c = Counter(hand.cards)
+    counts = [c for _, c in c.most_common()]
+    most = counts[0]
+    second = counts[1] if len(counts) > 1 else 0
+    return card_values(hand, most, second, VALUES)
+
+
+def key2(hand: Hand):
+    jokers = len([c for c in hand.cards if c == "J"])
+    without_jokers = hand.cards.replace("J", "")
+    c = Counter(without_jokers)
+    counts = [c for _, c in c.most_common()]
+    most = (counts[0] if counts else 0) + jokers
+    second = counts[1] if len(counts) > 1 else 0
+    return card_values(hand, most, second, VALUES2)
+
+
+def solve(input: str):
+    hands = [Hand(line) for line in input.split("\n")]
+    part1 = sum((i + 1) * hand.bid for i, hand in enumerate(sorted(hands, key=key)))
+    part2 = sum((i + 1) * hand.bid for i, hand in enumerate(sorted(hands, key=key2)))
+    return Answer(part1, part2)
