9a1616f53db6cf059199ea2517817f20131be5b1
Barnaby Gray <barnaby@pickle.me.uk>
2023-12-17 18:16:29 +0000

    Day 12 (finally!)

--- aoc2023/day12.py
+++ aoc2023/day12.py
@@ -1,38 +1,58 @@
-from itertools import combinations
+from functools import cache
 from .utils import Answer
 
 
-def arrangements(line: str):
-    pattern, numbers = line.split(" ")
+@cache
+def count(numbers: tuple, dots: int, sets: int, length: int) -> int:
+    ret = 0
+    n = numbers[0]
+    remain = length - sum(numbers) - len(numbers) + 1
+    bits = (1 << n) - 1
+    for i in range(remain + 1):
+        if sets & (1 << i) - 1 or dots & bits or sets & (1 << i + n):
+            bits <<= 1
+            continue
+
+        shift = n + i + 1
+        if len(numbers) == 1:
+            if not sets >> shift:
+                ret += 1
+        else:
+            ret += count(numbers[1:], dots >> shift, sets >> shift, length - shift)
+
+        bits <<= 1
+
+    return ret
+
+
+def arrangements(pattern: str, numbers: list[int]) -> int:
     pattern = pattern.strip(".").rstrip(".")
-    blanks = [1 << i for i, c in enumerate(pattern) if c == "?"]
+    dots = sum(1 << i for i, c in enumerate(pattern) if c == ".")
+    sets = sum(1 << i for i, c in enumerate(pattern) if c == "#")
+
+    return count(tuple(numbers), dots, sets, len(pattern))
+
+
+def part1(line: str):
+    pattern, numbers = parse(line)
+    return arrangements(pattern, numbers)
+
+
+def part2(line: str):
+    pattern, numbers = parse(line)
+    pattern = "?".join([pattern] * 5)
+    numbers = numbers * 5
+    return arrangements(pattern, numbers)
+
+
+def parse(line: str):
+    pattern, numbers = line.split(" ")
     numbers = [int(n) for n in numbers.split(",")]
-    missing = sum(numbers) - pattern.count("#")
-    existing = sum(1 << i for i, c in enumerate(pattern) if c == "#")
-    count = 0
-
-    for comb in combinations(blanks, missing):
-        combined = sum(comb) + existing
-        pos = 0
-        while True:
-            # shift out zeros
-            while combined & 1 == 0:
-                combined >>= 1
-            n = 1
-            combined >>= 1
-            while combined & 1 == 1:
-                combined >>= 1
-                n += 1
-            if n != numbers[pos]:
-                break
-            pos += 1
-            if pos == len(numbers):
-                count += 1
-                break
-
-    return count
+    return pattern, numbers
 
 
 def solve(input: str):
-    part1 = sum(map(arrangements, input.split("\n")))
-    return Answer(part1, 0)
+    lines = input.split("\n")
+    p1 = sum(part1(line) for line in lines)
+    p2 = sum(part2(line) for line in lines)
+    return Answer(p1, p2)
--- tests/test_day12.py
+++ tests/test_day12.py
@@ -16,8 +16,19 @@ from aoc2023 import day12
     ],
 )
 def test_01(line, answer):
-    assert day12.arrangements(line) == answer
+    assert day12.part1(line) == answer
 
 
-def test_02():
-    assert day12.solve(testdata).part2 == None
+@pytest.mark.parametrize(
+    ("line", "answer"),
+    [
+        ("???.### 1,1,3", 1),
+        (".??..??...?##. 1,1,3", 16384),
+        ("?#?#?#?#?#?#?#? 1,3,1,6", 1),
+        ("????.#...#... 4,1,1", 16),
+        ("????.######..#####. 1,6,5", 2500),
+        ("?###???????? 3,2,1", 506250),
+    ],
+)
+def test_02(line, answer):
+    assert day12.part2(line) == answer
